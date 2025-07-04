e5ccc18c59aa427222c8d16f122883375434e401
Abhi-12126 <nagireddi.abhishek@gmail.com>
2025-07-04 13:45:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import requests
 import io
 from PIL import Image
 import os
-import sounddevice as sd
-import soundfile as sf
+# import sounddevice as sd
+# import soundfile as sf
 import tempfile
 from PyPDF2 import PdfReader
 import fitz  # PyMuPDF
